43324b7a76bb0bf4bd2009c69cfa7a2116dc89ed
Garrett Hays <GarrettlHays@gmail.com>
2022-06-28 16:49:21 -0700

    in progress files
    Co-authored-by: Alex McKnight <alex.mcknight11@gmail.com>
    Co-authored-by: Kyle Chinick <kylechinick@icloud.com>

--- src/index.js
+++ src/index.js
@@ -8,20 +8,38 @@ import DinoService from './utility.js'
 
 $(document).ready(function() {
   $('#dino-ipsum-button').click(function() {
+    $('#gameSetup').hide();
+    $('#gamePlay').show();
     let wordCount = 1;
     let promise = DinoService.getDino(wordCount);
+    let lives = 9;
+    let answerArray = [];
     promise.then(function(response) {
       const dinoPoo = JSON.parse(response);
-      const dinoString = new String(`${dinoPoo}`)
-      console.log(dinoString);
+      const dinoString = new String(`${dinoPoo}`).toLowerCase();
       for (let i = 0; i < dinoString.length; i++) {
-        
-        dinoArray[i] = "_";
+        answerArray[i] = "x";
+  }
+
+  $('#dinoGuess').click(function() {
+    let failCounter = 0;
+    for (let i = 0; i < dinoString.length; i++) {
+      let userGuess = $('#dinoInput').val();
+      console.log(dinoString);
+      if (userGuess === dinoString[i]) {
+        answerArray[i] = userGuess;
+        // display correctly guessed letters
+      } else {
+        failCounter++;
       }
-      $('.show-ipsum').text(dinoPoo);
-    //   $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    // }, function(error) {
-    //   $('.showErrors').text(`There was an error processing your request: ${error}`);
+      if (failCounter >= dinoString.length) {
+        lives--;
+        failCounter = 0;
+      }
+      console.log(lives);
+      console.log(answerArray);
+    }
+    })
     });
   });
 });
@@ -31,6 +49,9 @@ $(document).ready(function() {
 // -- API gets called, we get dino name [x]
 // separate dino name to individual letters [x]
 // prompt user to input letter guess [x]
-// for loop through array of individualized dino name - check for guess
-// return true or false
-// display correctly guessed letters
+// for loop through array of individualized dino name - check for guess [x]
+// update answerArray and/or lives counter [x]
+// display correctly guessed letters 
+// display lives left
+// define the endgame (win or lose)
+// make less blue
